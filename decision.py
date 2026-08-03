33f9e0f8d92269d8c17f929ba572f59537339cff
Anshdeep Singh <anshdeep7988@gmail.com>
2026-08-03 21:16:25 +0530

    Implement DecisionEngine for task handling

---
+++ decision.py
@@ -0,0 +1,32 @@
+class DecisionEngine:
+
+    def decide(self, state):
+
+        task = state.get_task().lower()
+
+        if task.startswith("remember"):
+            return "SAVE_MEMORY"
+
+        elif "last question" in task:
+            return "GET_PREVIOUS_QUESTION"
+        
+        elif "what did i just say" in task:
+            return "GET_LAST_USER_MESSAGE"
+
+        elif "what was your last reply" in task:
+            return "GET_LAST_ASSISTANT_MESSAGE"
+
+        elif task.startswith("what is my"):
+            return "RECALL_MEMORY"
+
+
+        elif task.startswith("open"):
+            return "OPEN_APP"
+
+        elif task.startswith("search"):
+            return "SEARCH"
+
+        else:
+            return "CHAT"
+
+        
