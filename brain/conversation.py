22704be76cb060469dd4a2c4b45734a77122d3f4
Anshdeep Singh <anshdeep7988@gmail.com>
2026-08-22 20:48:19 +0530

    changed conversation.py

--- brain/conversation.py
+++ brain/conversation.py
@@ -31,7 +31,7 @@ class Conversation:
     # -----------------------------
     def get_messages(self):
         return self.messages
-    
+
 
     # -----------------------------
     # Previous User Question
