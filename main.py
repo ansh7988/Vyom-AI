0e88870f2191b946928ed4e58ced7fb469acf439
Anshdeep Singh <anshdeep7988@gmail.com>
2026-08-26 21:03:57 +0530

    main file changed

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ def listen():
 
     with sr.Microphone() as source:
         print("Listening...")
-
         recognizer.adjust_for_ambient_noise(source, duration=0.5)
         audio = recognizer.listen(source)
     try:
