dd02722928e4556c18d8d806833703aad02128ac
Anshdeep Singh <anshdeep7988@gmail.com>
2026-08-26 21:03:23 +0530

    main file changed

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ def listen():
         recognizer.adjust_for_ambient_noise(source, duration=0.5)
 
         audio = recognizer.listen(source)
-
     try:
         text = recognizer.recognize_google(audio)
         print("You:", text)
